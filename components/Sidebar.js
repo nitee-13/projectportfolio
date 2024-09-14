6a7b3cf79bcea5d8e24e0179aed13acc2e48d3db
Niteeq Sheik <nit.sheik@gmail.com>
2024-09-13 21:02:04 -0700

    SideBar Added

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,58 +7,47 @@ import { Menu, Button } from 'antd';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-const items = [
-  {
-    key: '/',
-    label: <Link href="/">Home</Link>,
-    icon: <MailOutlined />,
-  },
-  {
-    key: '/about',
-    label: <Link href="/about">About</Link>,
-    icon: <AppstoreOutlined />,
-  },
-  {
-    key: '/projects',
-    label: <Link href="/projects">Projects</Link>,
-    icon: <SettingOutlined />,
-  },
-  {
-    key: '/contact',
-    label: <Link href="/contact">Contact</Link>,
-    icon: <MailOutlined />,
-  },
-];
-
-const Sidebar = () => {
-  const pathname = usePathname();
-  const [collapsed, setCollapsed] = useState(false)
-  const toggleCollapsed = () => { 
-    setCollapsed(!collapsed)
-  }
-  return (
-    <div 
-    style={{
-        width:256,
-    }}>
-        <Button 
-        type = "primary"
-        onClick={toggleCollapsed}
-        style={{
-            marginBottom:16,
-        }}>
-            {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-        </Button>
-   
-    <Menu
-      selectedKeys={[pathname]}
-      mode="inline"
-      theme="dark"
-      inlineCollapsed={collapsed}
-      items={items}
-    /> 
-    </div>
-  );
+const Sidebar = () =>{
+    const pathname = usePathname();
+    const [collapsed, setCollapsed] = useState(false)
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed)
+    };
+    const items = [
+        {
+            key : '/', 
+            label:<Link href='/'>Home</Link>,
+            icon:<MailOutlined />,
+        },
+        {
+            key:'/about',
+            label:<Link href='/about'>About</Link>,
+            icon:<SettingOutlined/>,
+        },
+        {
+            key:'/contact',
+            label:<Link href='/contact'>Contact</Link>,
+            icon:<AppstoreOutlined/>,
+        },
+        {
+            key:'collapse',
+            label:collapsed?'Expand':'Collapse',
+            icon:collapsed?<MenuUnfoldOutlined/>:<MenuFoldOutlined/>,
+            onClick:toggleCollapsed,
+        },
+    ];
+    return (
+        <div style = {{width:collapsed?80:256, transition:'0.3s'}}>
+            <Menu 
+            style= {{ height:'100vh'}}
+            selectedKeys={[pathname]}
+            mode="inline"
+            theme="dark"
+            inlineCollapsed={collapsed}
+            items={items}
+            />
+        </div>
+    )
 };
 
-export default Sidebar;
+export default Sidebar
